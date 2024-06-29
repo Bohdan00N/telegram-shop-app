65129e96b7814a917a15f2268a916d786e4479c4
Bohdan Nalyvkin <nalivkin.bog@gmail.com>
2024-06-29 17:40:14 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ function App() {
 
   return (
     <div className={css.app}>
-      <Button onClick={onToggleButton}></Button>
+      <Button onClick={onToggleButton}Toggle></Button>
     </div>
   );
 }
