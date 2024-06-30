be7dc05ce23a8018c534d53988404b7768a1a896
Bohdan Nalyvkin <nalivkin.bog@gmail.com>
2024-06-30 13:53:08 +0300

    3

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -1,8 +1,64 @@
-import css from './form.module.scss'
-import React from 'react'
+import { useTG } from "../../hooks/useTG";
+import css from "./form.module.scss";
+import React, { useEffect, useState } from "react";
 
 export const Form = () => {
+  const [city, setCity] = useState("");
+  const [street, setStreet] = useState("");
+  const [subject, setSubject] = useState("physical");
+  const { tg } = useTG();
+
+  useEffect(() => {
+    tg.MainButton.setParams({
+      text: "Відправити",
+    });
+  }, []);
+
+  useEffect(() => {
+    if (!city || !street) {
+      tg.MainButton.hide();
+    } else {
+      tg.MainButton.show();
+    }
+  }, [city, street]);
+
+  const onChangeCity = (e) => {
+    setCity(e.target.value);
+  };
+  const onChangeStreet = (e) => {
+    setStreet(e.target.value);
+  };
+  const onChangeSubject = (e) => {
+    setSubject(e.target.value);
+  };
+
   return (
-    <div className={css.form}>Form</div>
-  )
-}
+    <div className={css.form}>
+      <h3>Ваші данні</h3>
+      <input
+        className={css.input}
+        type="text"
+        placeholder="Місто"
+        value={city}
+        onChange={onChangeCity}
+      />
+      <input
+        className={css.input}
+        type="text"
+        placeholder="Вулиця"
+        value={street}
+        onChange={onChangeStreet}
+      />
+      <select
+        className={css.select}
+        value={subject}
+        onChange={onChangeSubject}
+        name=""
+        id=""
+      >
+        <option value="physical">Fiz</option>
+        <option value="legal">Yur</option>
+      </select>
+    </div>
+  );
+};
