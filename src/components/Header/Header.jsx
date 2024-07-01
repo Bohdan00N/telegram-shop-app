cf26cef8076fd49eeb6754f19ebb7707bda954ad
Bohdan Nalyvkin <nalivkin.bog@gmail.com>
2024-07-01 15:53:32 +0300

    1

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -3,10 +3,9 @@ import css from "./Header.module.scss";
 import { useTG } from "../../hooks/useTG";
 
 export const Header = () => {
-  const { user, onClose } = useTG();
+  const { user } = useTG();
   return (
     <div className={css.header}>
-      <button onClick={onClose}>Закрити</button>
       <span className={css.username}>{user?.username}</span>
     </div>
   );
--- src/components/ProductList/ProductList.jsx
+++ src/components/ProductList/ProductList.jsx
@@ -1,90 +1,122 @@
-import React, {useState, useCallback, useEffect} from 'react';
-import css from './productList.module.scss';
+import React, { useState, useCallback, useEffect } from "react";
+import css from "./productList.module.scss";
 import ProductItem from "../ProductItem/ProductItem";
-import {useTG} from "../../hooks/useTG";
+import { useTG } from "../../hooks/useTG";
 
 const products = [
-    {id: '1', title: 'Джинсы', price: 5000, description: 'Синего цвета, прямые'},
-    {id: '2', title: 'Куртка', price: 12000, description: 'Зеленого цвета, теплая'},
-    {id: '3', title: 'Джинсы 2', price: 5000, description: 'Синего цвета, прямые'},
-    {id: '4', title: 'Куртка 8', price: 122, description: 'Зеленого цвета, теплая'},
-    {id: '5', title: 'Джинсы 3', price: 5000, description: 'Синего цвета, прямые'},
-    {id: '6', title: 'Куртка 7', price: 600, description: 'Зеленого цвета, теплая'},
-    {id: '7', title: 'Джинсы 4', price: 5500, description: 'Синего цвета, прямые'},
-    {id: '8', title: 'Куртка 5', price: 12000, description: 'Зеленого цвета, теплая'},
-]
+  {
+    id: "1",
+    title: "Джинсы",
+    price: 5000,
+    description: "Синего цвета, прямые",
+  },
+  {
+    id: "2",
+    title: "Куртка",
+    price: 12000,
+    description: "Зеленого цвета, теплая",
+  },
+  {
+    id: "3",
+    title: "Джинсы 2",
+    price: 5000,
+    description: "Синего цвета, прямые",
+  },
+  {
+    id: "4",
+    title: "Куртка 8",
+    price: 122,
+    description: "Зеленого цвета, теплая",
+  },
+  {
+    id: "5",
+    title: "Джинсы 3",
+    price: 5000,
+    description: "Синего цвета, прямые",
+  },
+  {
+    id: "6",
+    title: "Куртка 7",
+    price: 600,
+    description: "Зеленого цвета, теплая",
+  },
+  {
+    id: "7",
+    title: "Джинсы 4",
+    price: 5500,
+    description: "Синего цвета, прямые",
+  },
+  {
+    id: "8",
+    title: "Куртка 5",
+    price: 12000,
+    description: "Зеленого цвета, теплая",
+  },
+];
 
 const getTotalPrice = (items = []) => {
-    return items.reduce((acc, item) => {
-        return acc += item.price
-    }, 0)
-}
-
+  return items.reduce((acc, item) => {
+    return (acc += item.price);
+  }, 0);
+};
 
 const ProductList = () => {
-    const [addedItems, setAddedItems] = useState([]);
-    const {tg, queryId} = useTG();
-console.log( queryId);
-    const onSendData = useCallback(() => {
-        const data = {
-            products: addedItems,
-            totalPrice: getTotalPrice(addedItems),
-            queryId,
-        }
-        fetch('http://localhost:3001/order', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-            },
-            body: JSON.stringify(data)
-        })
-    }, [addedItems, queryId])
+  const [addedItems, setAddedItems] = useState([]);
+  const { tg, queryId } = useTG();
+  console.log(queryId);
+  const onSendData = useCallback(() => {
+    const data = {
+      products: addedItems,
+      totalPrice: getTotalPrice(addedItems),
+      queryId,
+    };
+    fetch("http://localhost:3001/order", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(data),
+    });
+  }, [addedItems, queryId]);
 
-    useEffect(() => {
-        console.log('Setting up event listener for mainButtonClicked');
-        tg.onEvent('mainButtonClicked', onSendData)
-        return () => {
-            console.log('Removing event listener for mainButtonClicked');
-            tg.offEvent('mainButtonClicked', onSendData)
-        }
-    }, [onSendData, tg])
+  useEffect(() => {
+    console.log("Setting up event listener for mainButtonClicked");
+    tg.onEvent("mainButtonClicked", onSendData);
+    return () => {
+      console.log("Removing event listener for mainButtonClicked");
+      tg.offEvent("mainButtonClicked", onSendData);
+    };
+  }, [onSendData, tg]);
 
-    const onAdd = (product) => {
-        const alreadyAdded = addedItems.find(item => item.id === product.id);
-        let newItems = [];
+  const onAdd = (product) => {
+    const alreadyAdded = addedItems.find((item) => item.id === product.id);
+    let newItems = [];
 
-        if(alreadyAdded) {
-            newItems = addedItems.filter(item => item.id !== product.id);
-        } else {
-            newItems = [...addedItems, product];
-        }
-
-        setAddedItems(newItems)
-
-        if(newItems.length === 0) {
-            tg.MainButton.hide();
-        } else {
-            tg.MainButton.show();
-            tg.MainButton.setParams({
-                text: `Купить ${getTotalPrice(newItems)}`
-            })
-        }
+    if (alreadyAdded) {
+      newItems = addedItems.filter((item) => item.id !== product.id);
+    } else {
+      newItems = [...addedItems, product];
     }
 
-    return (
-        <div className={css.list}>
-            {products.map(item => (
-                <ProductItem
-                    product={item}
-                    onAdd={onAdd}
-                    className={css.item}
-                />
-            ))}
-        <div className={css.btn}>
-            <button onClick={onSendData}>Сделать заказ</button>
-        </div>
-        </div>
-    );
+    setAddedItems(newItems);
+
+    if (newItems.length === 0) {
+      tg.MainButton.hide();
+    } else {
+      tg.MainButton.show();
+      tg.MainButton.setParams({
+        text: `Купить ${getTotalPrice(newItems)}`,
+      });
+    }
+  };
+
+  return (
+    <div className={css.list}>
+      {products.map((item) => (
+        <ProductItem product={item} onAdd={onAdd} className={css.item} />
+      ))}
+    </div>
+  );
 };
 
 export default ProductList;
