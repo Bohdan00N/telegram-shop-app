825054fecf487c4678dffff3d47f7f68eabb55bc
Bohdan Nalyvkin <nalivkin.bog@gmail.com>
2024-08-09 16:11:13 +0300

    Update Form.jsx

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -25,7 +25,7 @@ const Form = () => {
     };
 
     try {
-      const response = await fetch("https://sushi-tg.local/order", {
+      const response = await fetch("http://localhost:3001/order", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
